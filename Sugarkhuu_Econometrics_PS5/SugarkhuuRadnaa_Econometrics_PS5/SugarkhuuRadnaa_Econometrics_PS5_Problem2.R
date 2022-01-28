b79b5cc9e1bdc8ffd1ac60e227831139dd7a232d
Py4Econmn <Py4Econmn>
2022-01-28 15:54:45 +0100

    done

---
+++ Sugarkhuu_Econometrics_PS5/SugarkhuuRadnaa_Econometrics_PS5/SugarkhuuRadnaa_Econometrics_PS5_Problem2.R
@@ -0,0 +1,116 @@
+## libraries
+library("readxl")
+library(ggplot2)
+library(reshape2)
+
+# import
+source("utils.R")
+
+# data
+df <- read_excel("WDB.xlsx")
+
+# regression variables
+X <- as.matrix(cbind(df[c("water","rural","health_gdp")],intercept=1))
+Y <- as.matrix(df["mrate"])
+n <- length(Y)
+
+# results
+
+## a
+beta_est <- my_ols(X,Y)                    # beta OLS
+
+
+## b
+
+v_b <- vcov_beta(beta_est, X, Y)           # homosk. vcov matrix of betas
+v_b_het <- vcov_beta_het(beta_est, X, Y)   # heterosk. vcov matrix of betas
+
+# summary matrix
+rows <- c("beta_1","beta_2","beta_3","beta_4")
+cols <- c('est','std',"null","lower","upper",'p_value')
+mat_hom <- matrix(nrow = length(rows), ncol = length(cols))
+rownames(mat_hom) <- rows
+colnames(mat_hom) <- cols
+mat_het <- mat_hom
+
+for(j in 1:length(beta_est)){
+  beta_test <- 0
+  beta_est_i <- beta_est[j]
+  s_beta_hom <- sqrt(v_b[j,j])
+  s_beta_het <- sqrt(v_b_het[j,j])
+
+  # confidence interval around 0
+  result_hom <- conf_interval(beta_true=beta_test, beta_est=beta_est_i,s_beta=s_beta_hom, n_size=n,alpha=0.05) # ,ifInt=FALSE  
+  result_het <- conf_interval(beta_true=beta_test, beta_est=beta_est_i,s_beta=s_beta_het, n_size=n,alpha=0.05) # ,ifInt=FALSE  
+
+  lower_hom  <- beta_test + result_hom[3]
+  upper_hom  <- beta_test + result_hom[4]
+  lower_het  <- beta_test + result_het[3]
+  upper_het  <- beta_test + result_het[4]
+  p_value_hom <- result_hom[5]
+  p_value_het <- result_het[5]
+
+  mat_hom[j,] <- c(beta_est[j],s_beta_hom,beta_test,lower_hom, upper_hom,p_value_hom)         # avg*2 estimates
+  mat_het[j,] <- c(beta_est[j],s_beta_het,beta_test,lower_het, upper_het,p_value_het)         # avg*2 estimates
+}
+
+print("Results for 2b: ")
+print(mat_hom)
+print(mat_het)
+
+## c - It is actually available in the above table. Here just to make it easier to see how it is done.
+alpha <- 0.05
+t_crit <- qt(1-alpha/2,length(Y)-4)
+beta_i <- 2
+beta_est_i  <- beta_est[beta_i]
+s_beta_hom <- sqrt(v_b[beta_i,beta_i])
+s_beta_het <- sqrt(v_b_het[beta_i,beta_i])
+lower_hom <- beta_est_i - t_crit*s_beta_hom
+upper_hom <- beta_est_i + t_crit*s_beta_hom
+lower_het <- beta_est_i - t_crit*s_beta_het
+upper_het <- beta_est_i + t_crit*s_beta_het
+
+
+print(paste("Lower bound of beta 2 (homosk.):   ",   round(lower_hom,3)))
+print(paste("Upper bound of beta 2 (homosk.):   ",   round(upper_hom,3)))
+print(paste("Lower bound of beta 2 (heterosk.):   ", round(lower_het,3)))
+print(paste("Upper bound of beta 2 (heterosk.):   ", round(upper_het,3)))
+
+
+
+## d Assuming homosk.
+cntr <- c(85,46,6,1)
+mrate_hat <- t(beta_est) %*% cntr
+sigma_hat_hom <- sqrt(t(cntr)%*%v_b%*%cntr)
+sigma_hat_het <- sqrt(t(cntr)%*%v_b_het%*%cntr)
+alpha <- 0.05
+t_crit <- qt(1-alpha/2,length(Y)-4)
+lower_theta_hom <- mrate_hat - t_crit*sigma_hat_hom
+upper_theta_hom <- mrate_hat + t_crit*sigma_hat_hom
+lower_theta_het <- mrate_hat - t_crit*sigma_hat_het
+upper_theta_het <- mrate_hat + t_crit*sigma_hat_het
+
+
+## e
+cntr_0 <- c(85,46,6,0)
+X <- X - matrix(cntr_0, nrow=dim(X)[1], ncol=length(cntr), byrow=TRUE)
+beta_est_theta <- my_ols(X,Y)                    # beta OLS
+
+# assert 
+stopifnot(round((t(beta_est) %*% cntr),3) == round(beta_est_theta[4],3))
+
+## f
+R_vec <- c(1,10,0,0)
+betas <- beta_est
+
+theta_test <- 0
+theta_hat <- t(R_vec) %*% betas
+
+# v_theta
+v_theta_hom <- (t(R_vec) %*% v_b %*% R_vec)
+v_theta_het <- (t(R_vec) %*% v_b_het %*% R_vec)
+
+wald_stat_hom <- (theta_hat- theta_test)%*%solve(v_theta_hom)%*%(theta_hat- theta_test)
+wald_stat_het <- (theta_hat- theta_test)%*%solve(v_theta_het)%*%(theta_hat- theta_test)
+
+wald_crit <- qchisq(0.95,1)
---
+++ Sugarkhuu_Econometrics_PS5/SugarkhuuRadnaa_Econometrics_PS5/utils.R
@@ -0,0 +1,110 @@
+conf_interval <- function(beta_true=2, beta_est=2,s_beta=1, n_size=1,alpha=0.05,ifInt=TRUE) {
+#' Calculate actual coverage probability for exponential distribution
+#' theta - mean value of the distribution
+#' it    - number of different draws
+  n <- n_size
+  k <- 3
+
+
+  t_crit <- qt(1-alpha/2,n-k) # percentile of t dist with n-3 df
+  
+  if(ifInt){
+    conf_int <- beta_est+c(-1,1)*t_crit*s_beta 
+    # check if within the interval: Boolean
+    ifInside <- rbind(conf_int[1] <= beta_true & beta_true <= conf_int[2])
+  } else {
+    conf_int <- beta_true+c(-1,1)*t_crit*s_beta 
+    # check if within the interval: Boolean
+    ifInside <- rbind(conf_int[1] <= beta_est & beta_est <= conf_int[2])
+  }
+  conf_int_len <- conf_int[2]-conf_int[1]
+  
+  t_stat <- abs((beta_est - beta_true)/(s_beta)) 
+  p_value <- 2*(1-pt(t_stat,n-k))
+
+  return(c(ifInside,conf_int_len,conf_int,p_value))
+}
+
+vcov_beta <- function(beta_est, X, Y){
+  # calculate R squared given beta, X and Y
+  n <- length(Y)
+  k <- length(beta_est)
+  Y_hat     <- X%*%beta_est
+  e_hat     <- Y_hat - Y 
+  var_ehat  <- var(e_hat)
+  v_beta <- n/(n-k)*var_ehat[1]*solve((t(X) %*% X))
+  
+  return(v_beta)
+} 
+
+vcov_beta_het <- function(beta_est, X, Y){
+  # calculate R squared given beta, X and Y
+  n <- length(Y)
+  k <- length(beta_est)
+  Y_hat     <- X%*%beta_est
+  e_hat     <- Y_hat - Y 
+  D_hat     <- diag(diag(e_hat %*% t(e_hat)))
+  v_beta_het <- n/(n-k)*solve(t(X) %*% X) %*% (t(X) %*% D_hat %*% X) %*% solve(t(X) %*% X)
+  
+  return(v_beta_het)
+} 
+
+sigma_error <- function(beta_est, X, Y){
+  # calculate sigma of error given beta, X and Y
+  Y_hat  <- X%*%beta_est
+  e_hat  <- Y_hat - Y 
+  sigma_ehat <- sqrt(mean(e_hat^2))
+ 
+  return(sigma_ehat)
+} 
+
+r_squared <- function(beta_est, X, Y){
+  # calculate R squared given beta, X and Y
+  Y_hat  <- X%*%beta_est
+  EY_hat <- mean(Y_hat) # <=> EY
+  EY     <- mean(Y)
+  r2     <- sum((Y_hat-EY_hat)^2)/sum((Y-EY)^2)
+  
+  return(r2)
+} 
+
+my_ols <- function(X,Y){
+# OLS estimates given X, Y
+  
+    beta_est <- solve((t(X) %*% X)) %*% (t(X) %*% Y) # estimate the betas
+  
+  return(beta_est)
+
+}
+
+my_estimator <- function(rho=0.7,n=1000) {
+  # return estimated betas and r squared for a random sample
+  
+  beta <- c(2,1,-1)
+  VCOV_mat <- data.frame(c(1, rho),c(rho,1)) # of Xs
+  MEAN_mat <- c(2,-3) # c(10,100)            # of Xs
+  A = as.matrix(VCOV_mat)                    # just rename
+  q = c(dim(A)[2])                           # number of X variables
+  
+  r_chol <- chol(A)                          # Cholesky decomp. of A
+  r_mat <- matrix(rnorm(n*q), ncol=q)        # draws from standard normal dist. of size nxq
+  
+  X_0 <- r_mat %*% r_chol                    # random sample with demeaned Xs
+  X   <-  X_0 + matrix(MEAN_mat, nrow=n, ncol=q, byrow=TRUE) # add respective means to the demeaned Xs
+  X   <- cbind(X,intercept=1);                 # add the intercept
+  e   <- runif(n,-2,2)                         # random error draws
+  
+  Y <- X %*% beta + e                        # draws of Y. Now, the observed values. 
+  
+  beta_est <- my_ols(X,Y) # estimate the betas
+  r2 <- r_squared(beta_est, X, Y)            
+  v_b <- vcov_beta(beta_est, X, Y)           
+  s_beta <- sqrt(v_b[1,1])               # std of beta1
+  ifInside <- conf_interval(beta_est=beta_est[1],s_beta=s_beta, n_size=n)  # confidence interval
+  ifHypo2True <- conf_interval(beta_true=2, beta_est=beta_est[1],s_beta=s_beta, n_size=n,alpha=0.01,ifInt=FALSE)     # hypothesis testing 
+  ifHypo18True <- conf_interval(beta_true=1.8, beta_est=beta_est[1],s_beta=s_beta, n_size=n,alpha=0.01,ifInt=FALSE)  # hypothesis testing  
+  
+  # betas, R2, if in interval, interval length,  if null true, if null true
+  return(c(beta_est, r2, ifInside[1],ifInside[2],1-ifHypo2True[1],1-ifHypo18True[1]))
+  
+}
